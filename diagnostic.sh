a631a05a2c25a9aee99188fb176f804b461a5a87
Crono1995 <82237306+Crono1995@users.noreply.github.com>
2021-04-09 14:53:02 -0600

    Update diagnostic.sh

--- diagnostic.sh
+++ diagnostic.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "********AZURE-CLOUD***********"
+echo "********AZURE-CLOUD***********"
+echo "********AZURE-CLOUD***********"
 echo "------------------------------"
 echo "1.-------CENTOS / REDHAT------"
 echo "2.-------DEBIAN---------------"
@@ -12,7 +14,7 @@ echo
 read -p "SECLECT AN OPTION:  " OPTION
 
 case "$OPTION" in
-	1)echo -e "\n\n\n-----SSHD SERVICE STATUS-----\n\n" >> /tmp/diagnostic ; systemctl status sshd >> /tmp/diagnostic;
+	1)echo -e "\n\n\n-----SSHD SERVICE STATUS-----\n\n" > /tmp/diagnostic ; systemctl status sshd >> /tmp/diagnostic;
 	echo -e "\n\n\n-----SSHD_CONFIG-------------\n\n" >> /tmp/diagnostic ; cat /etc/ssh/sshd_config >> /tmp/diagnostic ;
 	echo -e "\n\n\n-----NETWORK-MANAGER---------\n\n" >> /tmp/diagnostic ; systemctl status NetworkManager >> /tmp/diagnostic ;
 	echo -e "\n\n\n-----CLOUD-INIT--------------\n\n" >> /tmp/diagnostic ; systemctl status cloud-init.service >> /tmp/diagnostic ;
@@ -27,11 +29,11 @@ case "$OPTION" in
 	echo -e "\n\n\n-----SELINUX STATUS----------\n\n" >> /tmp/diagnostic ; sestatus >> /tmp/diagnostic ; 
 	echo -e "\n\n\n-----FIREWALLD SERVICE-------\n\n" >> /tmp/diagnostic ; systemctl status firewalld >> /tmp/diagnostic ; 
 	echo -e "\n\n\n-----FIREWALLD RULES---------\n\n" >> /tmp/diagnostic ; firewall-cmd --list-all >> /tmp/diagnostic ; 
-	echo -e "\n\n\n-----IP TABLES---------------\n\n" >> /tmp/diagnostic ; iptables -L >> /tmp/diagnostic ; 
+	echo -e "\n\n\n-----IP TABLES---------------\n\n" >> /tmp/diagnostic ; iptables -L >> /tmp/diagnostic ; clear ;
 	cat /tmp/diagnostic
 	;;
 	
-	2)echo -e "\n\n\n-----SSHD SERVICE STATUS:-----\n\n" >> /tmp/diagnostic ; systemctl status sshd >> /tmp/diagnostic;
+	2)echo -e "\n\n\n-----SSHD SERVICE STATUS:-----\n\n" > /tmp/diagnostic ; systemctl status sshd >> /tmp/diagnostic;
 	echo -e "\n\n\n-----SSHD_CONFIG:-------------\n\n" >> /tmp/diagnostic ; cat /etc/ssh/sshd_config >> /tmp/diagnostic ; 
 	echo -e "\n\n\n-----HOSTS.DENY:--------------\n\n" >> /tmp/diagnostic ; cat /etc/hosts.deny >> /tmp/diagnostic ;
 	echo -e "\n\n\n-----WALINUXAGENT STATUS:-----\n\n" >> /tmp/diagnostic ; systemctl status walinuxagent >> /tmp/diagnostic ; 
@@ -41,11 +43,11 @@ case "$OPTION" in
 	echo -e "\n\n\n-----ROUTING TABLE:-----------\n\n" >> /tmp/diagnostic ; route -n >> /tmp/diagnostic ; 
 	echo -e "\n\n\n-----RESOLV.CONF:-------------\n\n" >> /tmp/diagnostic ; cat /etc/resolv.conf >> /tmp/diagnostic ; 
 	echo -e "\n\n\n-----UFW STATUS---------------\n\n" >> /tmp/diagnostic ; ufw status >> /tmp/diagnostic ; 
-	echo -e "\n\n\n-----IP TABLES----------------\n\n" >> /tmp/diagnostic ; iptables -L >> /tmp/diagnostic ; 
+	echo -e "\n\n\n-----IP TABLES----------------\n\n" >> /tmp/diagnostic ; iptables -L >> /tmp/diagnostic ; clear ;
 	cat /tmp/diagnostic
 	;;
 
-	3)	echo -e "\n\n\n-----SSHD SERVICE STATUS:-----\n\n" >> /tmp/diagnostic ; systemctl status sshd >> /tmp/diagnostic; 
+	3)echo -e "\n\n\n-----SSHD SERVICE STATUS:-----\n\n" > /tmp/diagnostic ; systemctl status sshd >> /tmp/diagnostic; 
 	echo -e "\n\n\n-----SSHD_CONFIG:-------------\n\n" >> /tmp/diagnostic ; cat /etc/ssh/sshd_config >> /tmp/diagnostic ; 
 	echo -e "\n\n\n-----HOSTS.ALLOW:-------------\n\n" >> /tmp/diagnostic ; cat /etc/hosts.allow >> /tmp/diagnostic ;
 	echo -e "\n\n\n-----WALINUXAGENT STATUS:-----\n\n" >> /tmp/diagnostic ; systemctl status waagent >> /tmp/diagnostic ; 
@@ -56,11 +58,11 @@ case "$OPTION" in
 	echo -e "\n\n\n-----RESOLV.CONF:-------------\n\n" >> /tmp/diagnostic ; cat /etc/resolv.conf >> /tmp/diagnostic ; 
 	echo -e "\n\n\n-----IP TABLES----------------\n\n" >> /tmp/diagnostic ; iptables -L >> /tmp/diagnostic ; 
 	echo -e "\n\n\n-----FIREWALLD SERVICE--------\n\n" >> /tmp/diagnostic ; systemctl status firewalld >> /tmp/diagnostic ; 
-	echo -e "\n\n\n-----FIREWALLD RULES----------\n\n" >> /tmp/diagnostic ; firewall-cmd --list-all >> /tmp/diagnostic ; 
+	echo -e "\n\n\n-----FIREWALLD RULES----------\n\n" >> /tmp/diagnostic ; firewall-cmd --list-all >> /tmp/diagnostic ; clear ;
 	cat /tmp/diagnostic
 	;;
 
-	4)	echo -e "\n\n\n-----SSHD SERVICE STATUS:-----\n\n" >> /tmp/diagnostic ; systemctl status sshd >> /tmp/diagnostic; 
+	4)echo -e "\n\n\n-----SSHD SERVICE STATUS:-----\n\n" > /tmp/diagnostic ; systemctl status sshd >> /tmp/diagnostic; 
 	echo -e "\n\n\n-----SSHD_CONFIG:-------------\n\n" >> /tmp/diagnostic ; cat /etc/ssh/sshd_config >> /tmp/diagnostic ; 
 	echo -e "\n\n\n-----HOSTS.DENY:--------------\n\n" >> /tmp/diagnostic ; cat /etc/hosts.deny >> /tmp/diagnostic ; 
 	echo -e "\n\n\n-----WALINUXAGENT STATUS:-----\n\n" >> /tmp/diagnostic ; systemctl status waagent >> /tmp/diagnostic ; 
@@ -71,12 +73,12 @@ case "$OPTION" in
 	echo -e "\n\n\n-----RESOLV.CONF:-------------\n\n" >> /tmp/diagnostic ; cat /etc/resolv.conf >> /tmp/diagnostic ; 
 	echo -e "\n\n\n-----IP TABLES----------------\n\n" >> /tmp/diagnostic ; iptables -L >> /tmp/diagnostic ; 
 	echo -e "\n\n\n-----FIREWALLD SERVICE--------\n\n" >> /tmp/diagnostic ; systemctl status firewalld >> /tmp/diagnostic ; 
-	echo -e "\n\n\n-----FIREWALLD RULES----------\n\n" >> /tmp/diagnostic ; firewall-cmd --list-all >> /tmp/diagnostic ; 
+	echo -e "\n\n\n-----FIREWALLD RULES----------\n\n" >> /tmp/diagnostic ; firewall-cmd --list-all >> /tmp/diagnostic ; clear ;
 	cat /tmp/diagnostic
 	;;
 
 
-	5)	echo -e "\n\n\n-----SSHD SERVICE STATUS-----\n\n" >> /tmp/diagnostic ; systemctl status sshd >> /tmp/diagnostic;
+	5)echo -e "\n\n\n-----SSHD SERVICE STATUS-----\n\n" > /tmp/diagnostic ; systemctl status sshd >> /tmp/diagnostic;
 	echo -e "\n\n\n-----SSHD_CONFIG-------------\n\n" >> /tmp/diagnostic ; cat /etc/ssh/sshd_config >> /tmp/diagnostic ;
 	echo -e "\n\n\n-----HOSTS-------------------\n\n" >> /tmp/diagnostic ; cat /etc/hosts >> /tmp/diagnostic ;
 	echo -e "\n\n\n-----WALINUXAGENT STATUS-----\n\n" >> /tmp/diagnostic ; systemctl status walinuxagent >> /tmp/diagnostic ;
@@ -86,7 +88,7 @@ case "$OPTION" in
 	echo -e "\n\n\n-----ROUTING TABLE-----------\n\n" >> /tmp/diagnostic ; route -n >> /tmp/diagnostic ;
 	echo -e "\n\n\n-----RESOLV.CONF-------------\n\n" >> /tmp/diagnostic ; cat /etc/resolv.conf >> /tmp/diagnostic ; 
 	echo -e "\n\n\n-----UFW STATUS--------------\n\n" >> /tmp/diagnostic ; ufw status verbose >> /tmp/diagnostic ; 
-	echo -e "\n\n\n-----IP TABLES---------------\n\n" >> /tmp/diagnostic ; iptables -L >> /tmp/diagnostic ; 
+	echo -e "\n\n\n-----IP TABLES---------------\n\n" >> /tmp/diagnostic ; iptables -L >> /tmp/diagnostic ; clear ;
 	cat /tmp/diagnostic
 	;;
 	
