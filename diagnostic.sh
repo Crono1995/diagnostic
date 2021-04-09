637316becd628f97e1fa44279fdd40ff02c0e918
Crono1995 <82237306+Crono1995@users.noreply.github.com>
2021-04-09 13:57:47 -0600

    Create diagnostic.sh

---
+++ diagnostic.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+echo "1.-------CENTOS / REDHAT------"
+echo "2.-------DEBIAN---------------"
+echo "3.-------ORACLE-LINUX---------"
+echo "4.-------SUSE-----------------"
+echo "5.-------UBUNTU---------------"
+echo
+read -p "\n-OPTION:  " OPTION
+
+case "$OPTION" in
+	1)echo -e "\n\n\n-----SSHD SERVICE STATUS-----\n\n" >> /tmp/diagnostic; systemctl status sshd >> /tmp/diagnostic;
+	echo -e "\n\n\n-----SSHD_CONFIG-------------\n\n" >> /tmp/diagnostic ; cat /etc/ssh/sshd_config >> /tmp/diagnostic ;
+	echo -e "\n\n\n-----NETWORK-MANAGER---------\n\n" >> /tmp/diagnostic ; systemctl status NetworkManager >> /tmp/diagnostic ;
+	echo -e "\n\n\n-----CLOUD-INIT--------------\n\n" >> /tmp/diagnostic ; systemctl status cloud-init.service >> /tmp/diagnostic ;
+	echo -e "\n\n\n-----HOST.ALLOW--------------\n\n" >> /tmp/diagnostic ; cat /etc/host.allow >> /tmp/diagnostic ;
+	echo -e "\n\n\n-----HOST.DENY---------------\n\n" >> /tmp/diagnostic ; cat /etc/host.deny >> /tmp/diagnostic ;
+	echo -e "\n\n\n-----WALINUXAGENT STATUS-----\n\n" >> /tmp/diagnostic ; systemctl status waagent >> /tmp/diagnostic ;
+	echo -e "\n\n\n-----WALINUXAGENT VERSION----\n\n" >> /tmp/diagnostic ; waagent --version >> /tmp/diagnostic ;
+	echo -e "\n\n\n-----CONNECTIVITY TEST-------\n\n" >> /tmp/diagnostic ; ping -c 4 8.8.8.8 >> /tmp/diagnostic ;
+	echo -e "\n\n\n-----NETSTAT-----------------\n\n" >> /tmp/diagnostic ; netstat -tulpn | grep LISTEN >> /tmp/diagnostic ;
+	echo -e "\n\n\n-----ROUTING TABLE-----------\n\n" >> /tmp/diagnostic ; route -n >> /tmp/diagnostic ;
+	echo -e "\n\n\n-----RESOLV.CONF-------------\n\n" >> /tmp/diagnostic ; cat /etc/resolv.conf >> /tmp/diagnostic ; 
+	echo -e "\n\n\n-----SELINUX STATUS----------\n\n" >> /tmp/diagnostic ; sestatus >> /tmp/diagnostic ; 
+	echo -e "\n\n\n-----FIREWALLD SERVICE-------\n\n" >> /tmp/diagnostic ; systemctl status firewalld >> /tmp/diagnostic ; 
+	echo -e "\n\n\n-----FIREWALLD RULES---------\n\n" >> /tmp/diagnostic ; firewall-cmd --list-all >> /tmp/diagnostic ; 
+	echo -e "\n\n\n-----IP TABLES---------------\n\n" >> /tmp/diagnostic ; iptables -L >> /tmp/diagnostic ; 
+	cat /tmp/diagnostic
+	;;
+	
+	2)echo -e "\n\n\n-----SSHD SERVICE STATUS:-----\n\n" >> /tmp/diagnostic; systemctl status sshd >> /tmp/diagnostic;
+	echo -e "\n\n\n-----SSHD_CONFIG:-------------\n\n" >> /tmp/diagnostic ; cat /etc/ssh/sshd_config >> /tmp/diagnostic ; 
+	echo -e "\n\n\n-----HOSTS.DENY:--------------\n\n" >> /tmp/diagnostic ; cat /etc/hosts.deny >> /tmp/diagnostic ;
+	echo -e "\n\n\n-----WALINUXAGENT STATUS:-----\n\n" >> /tmp/diagnostic ; systemctl status walinuxagent >> /tmp/diagnostic ; 
+	echo -e "\n\n\n-----WALINUXAGENT VERSION:----\n\n" >> /tmp/diagnostic ; waagent --version >> /tmp/diagnostic ; 
+	echo -e "\n\n\n-----CONNECTIVITY TEST:-------\n\n" >> /tmp/diagnostic ; ping -c 4 8.8.8.8 >> /tmp/diagnostic ; 
+	echo -e "\n\n\n-----NETSTAT:-----------------\n\n" >> /tmp/diagnostic ; netstat -tulpn | grep LISTEN >> /tmp/diagnostic ; 
+	echo -e "\n\n\n-----ROUTING TABLE:-----------\n\n" >> /tmp/diagnostic ; route -n >> /tmp/diagnostic ; 
+	echo -e "\n\n\n-----RESOLV.CONF:-------------\n\n" >> /tmp/diagnostic ; cat /etc/resolv.conf >> /tmp/diagnostic ; 
+	echo -e "\n\n\n-----UFW STATUS---------------\n\n" >> /tmp/diagnostic ; ufw status >> /tmp/diagnostic ; 
+	echo -e "\n\n\n-----IP TABLES----------------\n\n" >> /tmp/diagnostic ; iptables -L >> /tmp/diagnostic ; 
+	cat /tmp/diagnostic
+	;;
+
+	3)
+	echo -e "\n\n\n-----SSHD SERVICE STATUS:-----\n\n" >> /tmp/diagnostic; systemctl status sshd >> /tmp/diagnostic; 
+	echo -e "\n\n\n-----SSHD_CONFIG:-------------\n\n" >> /tmp/diagnostic ; cat /etc/ssh/sshd_config >> /tmp/diagnostic ; 
+	echo -e "\n\n\n-----HOSTS.ALLOW:-------------\n\n" >> /tmp/diagnostic ; cat /etc/hosts.allow >> /tmp/diagnostic ;
+	echo -e "\n\n\n-----WALINUXAGENT STATUS:-----\n\n" >> /tmp/diagnostic ; systemctl status waagent >> /tmp/diagnostic ; 
+	echo -e "\n\n\n-----WALINUXAGENT VERSION:----\n\n" >> /tmp/diagnostic ; waagent --version >> /tmp/diagnostic ; 
+	echo -e "\n\n\n-----CONNECTIVITY TEST:-------\n\n" >> /tmp/diagnostic ; ping -c 4 8.8.8.8 >> /tmp/diagnostic ; 
+	echo -e "\n\n\n-----NETSTAT:-----------------\n\n" >> /tmp/diagnostic ; netstat -tulpn | grep LISTEN >> /tmp/diagnostic ; 
+	echo -e "\n\n\n-----ROUTING TABLE:-----------\n\n" >> /tmp/diagnostic ; route -n >> /tmp/diagnostic ; 
+	echo -e "\n\n\n-----RESOLV.CONF:-------------\n\n" >> /tmp/diagnostic ; cat /etc/resolv.conf >> /tmp/diagnostic ; 
+	echo -e "\n\n\n-----IP TABLES----------------\n\n" >> /tmp/diagnostic ; iptables -L >> /tmp/diagnostic ; 
+	echo -e "\n\n\n-----FIREWALLD SERVICE--------\n\n" >> /tmp/diagnostic ; systemctl status firewalld >> /tmp/diagnostic ; 
+	echo -e "\n\n\n-----FIREWALLD RULES----------\n\n" >> /tmp/diagnostic ; firewall-cmd --list-all >> /tmp/diagnostic ; 
+	cat /tmp/diagnostic
+	;;
+
+	4)
+	echo -e "\n\n\n-----SSHD SERVICE STATUS:-----\n\n" >> /tmp/diagnostic; systemctl status sshd >> /tmp/diagnostic; 
+	echo -e "\n\n\n-----SSHD_CONFIG:-------------\n\n" >> /tmp/diagnostic ; cat /etc/ssh/sshd_config >> /tmp/diagnostic ; 
+	echo -e "\n\n\n-----HOSTS.DENY:--------------\n\n" >> /tmp/diagnostic ; cat /etc/hosts.deny >> /tmp/diagnostic ; 
+	echo -e "\n\n\n-----WALINUXAGENT STATUS:-----\n\n" >> /tmp/diagnostic ; systemctl status waagent >> /tmp/diagnostic ; 
+	echo -e "\n\n\n-----WALINUXAGENT VERSION:----\n\n" >> /tmp/diagnostic ; waagent --version >> /tmp/diagnostic ; 
+	echo -e "\n\n\n-----CONNECTIVITY TEST:-------\n\n" >> /tmp/diagnostic ; ping -c 4 8.8.8.8 >> /tmp/diagnostic ; 
+	echo -e "\n\n\n-----NETSTAT:-----------------\n\n" >> /tmp/diagnostic ; ss -tupln >> /tmp/diagnostic ; 
+	echo -e "\n\n\n-----ROUTING TABLE:-----------\n\n" >> /tmp/diagnostic ; ip route>> /tmp/diagnostic ; 
+	echo -e "\n\n\n-----RESOLV.CONF:-------------\n\n" >> /tmp/diagnostic ; cat /etc/resolv.conf >> /tmp/diagnostic ; 
+	echo -e "\n\n\n-----IP TABLES----------------\n\n" >> /tmp/diagnostic ; iptables -L >> /tmp/diagnostic ; 
+	echo -e "\n\n\n-----FIREWALLD SERVICE--------\n\n" >> /tmp/diagnostic ; systemctl status firewalld >> /tmp/diagnostic ; 
+	echo -e "\n\n\n-----FIREWALLD RULES----------\n\n" >> /tmp/diagnostic ; firewall-cmd --list-all >> /tmp/diagnostic ; 
+	cat /tmp/diagnostic
+	;;
+
+
+	5)
+	echo -e "\n\n\n-----SSHD SERVICE STATUS-----\n\n" >> /tmp/diagnostic; systemctl status sshd >> /tmp/diagnostic;
+	echo -e "\n\n\n-----SSHD_CONFIG-------------\n\n" >> /tmp/diagnostic ; cat /etc/ssh/sshd_config >> /tmp/diagnostic ;
+	echo -e "\n\n\n-----HOSTS-------------------\n\n" >> /tmp/diagnostic ; cat /etc/hosts >> /tmp/diagnostic ;
+	echo -e "\n\n\n-----WALINUXAGENT STATUS-----\n\n" >> /tmp/diagnostic ; systemctl status walinuxagent >> /tmp/diagnostic ;
+	echo -e "\n\n\n-----WALINUXAGENT VERSION----\n\n" >> /tmp/diagnostic ; waagent --version >> /tmp/diagnostic ;
+	echo -e "\n\n\n-----CONNECTIVITY TEST-------\n\n" >> /tmp/diagnostic ; ping -c 4 8.8.8.8 >> /tmp/diagnostic ;
+	echo -e "\n\n\n-----NETSTAT-----------------\n\n" >> /tmp/diagnostic ; netstat -tulpn | grep LISTEN >> /tmp/diagnostic ;
+	echo -e "\n\n\n-----ROUTING TABLE-----------\n\n" >> /tmp/diagnostic ; route -n >> /tmp/diagnostic ;
+	echo -e "\n\n\n-----RESOLV.CONF-------------\n\n" >> /tmp/diagnostic ; cat /etc/resolv.conf >> /tmp/diagnostic ; 
+	echo -e "\n\n\n-----UFW STATUS--------------\n\n" >> /tmp/diagnostic ; ufw status verbose >> /tmp/diagnostic ; 
+	echo -e "\n\n\n-----IP TABLES---------------\n\n" >> /tmp/diagnostic ; iptables -L >> /tmp/diagnostic ; 
+	cat /tmp/diagnostic
+	;;
+	
+	*)
+	echo "Invalid option"
+	;;
+esac
