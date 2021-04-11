14efe77036e3f12fb77b06fdfb22b16a6c834e71
Crono1995 <82237306+Crono1995@users.noreply.github.com>
2021-04-10 18:19:34 -0600

    Update diagnostic.sh

--- diagnostic.sh
+++ diagnostic.sh
@@ -32,7 +32,7 @@ case "$OPTION" in
 	echo -e "\n\n\n-----FIREWALLD SERVICE-------\n\n" 2>/dev/null >> /tmp/diagnostic ; systemctl status firewalld 2>/dev/null >> /tmp/diagnostic ; 
 	echo -e "\n\n\n-----FIREWALLD RULES---------\n\n" 2>/dev/null >> /tmp/diagnostic ; firewall-cmd --list-all 2>/dev/null >> /tmp/diagnostic ; 
 	echo -e "\n\n\n-----IP TABLES---------------\n\n" 2>/dev/null >> /tmp/diagnostic ; iptables -L 2>/dev/null >> /tmp/diagnostic ; 
-	echo -e "\n\n\n-----RHEL-RHUIS--------------\n\n" 2>/dev/null >> /tmp/diagnostic ; for x in rhui-1.microsoft.com rhui-4.microsoft.com rhui-3.microsoft.com ; do echo -e "\n$x" 2>/dev/null >> /tmp/diagnostic ; (echo >/dev/tcp/$x/443) && (echo "Open 443" || echo "Close 443") >> /tmp/diagnostic 2>&1 ; done ; clear ;
+	echo -e "\n\n\n-----RHEL-RHUIS--------------\n\n" 2>/dev/null >> /tmp/diagnostic ; for x in rhui-1.microsoft.com rhui-2.microsoft.com rhui-3.microsoft.com ; do echo -e "\n$x" 2>/dev/null >> /tmp/diagnostic ; (echo >/dev/tcp/$x/443) && (echo "Open 443" || echo "Close 443") >> /tmp/diagnostic 2>&1 ; done ; clear ;
 	cat /tmp/diagnostic ;
 	;;
 	
