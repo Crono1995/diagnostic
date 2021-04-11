c01bfcc20e936fe92b85acae77f64faf034e4b17
Crono1995 <82237306+Crono1995@users.noreply.github.com>
2021-04-10 18:07:21 -0600

    Update diagnostic.sh

--- diagnostic.sh
+++ diagnostic.sh
@@ -33,11 +33,7 @@ case "$OPTION" in
 	echo -e "\n\n\n-----FIREWALLD RULES---------\n\n" 2>/dev/null >> /tmp/diagnostic ; firewall-cmd --list-all 2>/dev/null >> /tmp/diagnostic ; 
 	echo -e "\n\n\n-----IP TABLES---------------\n\n" 2>/dev/null >> /tmp/diagnostic ; iptables -L 2>/dev/null >> /tmp/diagnostic ; clear ; /tmp/diagnostic ;
 	echo -e "\n\n\n-----RHEL-RHUIS--------------\n\n" 2>/dev/null >> /tmp/diagnostic ;
-	(echo >/dev/tcp/rhui-1.microsoft.com/443) && echo "Open 443" || echo "Close 443" >> /tmp/diagnostic 2>&1
-	(echo >/dev/tcp/rhui-2.microsoft.com/443) && echo "Open 443" || echo "Close 443" >> /tmp/diagnostic 2>&1
-	(echo >/dev/tcp/rhui-3.microsoft.com/443) && echo "Open 443" || echo "Close 443" >> /tmp/diagnostic 2>&1
-	for x in rhui-1.microsoft.com rhui-2.microsoft.com rhui-3.microsoft.com ; do echo -e "\n" && (echo >/dev/tcp/$x/443) && echo "Open 443" || echo "Close 443" | tee /tmp/diagnostic ; done
-	cat /tmp/diagnostic
+	for x in rhui-1.microsoft.com rhui-2.microsoft.com rhui-3.microsoft.com ; do echo -e "\n$x" 2>/dev/null >> /tmp/diagnostic ; (echo >/dev/tcp/rhui-1.microsoft.com/443) && (echo "Open 443" || echo "Close 443") >> /tmp/diagnostic 2>&1 ; done
 	;;
 	
 	2)echo -e "\n\n\n-----SSHD SERVICE STATUS:-----\n\n" 2>/dev/null > /tmp/diagnostic ; systemctl status sshd 2>/dev/null >> /tmp/diagnostic;
